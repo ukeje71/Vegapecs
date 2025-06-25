a5e2e50f84b23f5dbb806ded937531b44bf2dbc8
ukeje isaac <ukejeisaac71@gmail.com>
2025-06-25 13:05:44 +0630

    Feature :Started project by installing dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline text-center mt-10'>
+      Hello World 
+    </div>
+  )
+}
+
+export default App
